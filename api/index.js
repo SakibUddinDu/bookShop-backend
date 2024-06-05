55e3231ce26b2639b7989adcb788fb96342117eb
Mainuddinhasansakib <sakibuddin831@gmail.com>
2024-06-05 22:13:22 +0600

    jwt implemented

--- api/index.js
+++ api/index.js
@@ -1,12 +1,55 @@
 const express = require("express");
 const cors = require("cors");
 const { ObjectId, MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 const app = express();
 
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+function createToken(user) {
+  const token = jwt.sign(
+    {
+      email: user.email,
+    },
+    "secret",
+    { expiresIn: "7d" }
+  );
+  return token;
+}
+
+function verifyToken(req, res, next) {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(401).send("You are not authorized");
+    }
+
+    const verifiedUser = jwt.verify(token, "secret");
+    if (!verifiedUser?.email) {
+      return res.status(401).send("You are not authorized");
+    }
+    req.user = verifiedUser.email;
+    next();
+  } catch (error) {
+    console.error("Error verifying token:", error);
+    return res.status(401).send("You are not authorized");
+  }
+}
+
+// function verifyToken(req, res, next) {
+//   console.log(req.headers);
+//   const token = req.headers.authorization?.split(" ")[1];
+//   // console.log(token)
+//   const verifiedUser = jwt.verify(token, "secret");
+//   if (!verifiedUser?.email) {
+//     return res.send("You are not authorized");
+//   }
+//   req.user = verifiedUser.email;
+//   next();
+// }
 
 const uri =
   "mongodb+srv://sakibuddin831:nwlyZ3zg8wiyvkMw@cluster0.hqdjzcg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -33,6 +76,8 @@ async function run() {
     app.post("/user", async (req, res) => {
       try {
         const user = req.body;
+        const token =createToken(user);
+        console.log(token);
         const existingUser = await userCollection.findOne({
           email: user.email,
         });
@@ -41,6 +86,7 @@ async function run() {
           return res.send({
             status: "success",
             message: "Login success",
+            token
           });
         } else {
           // User does not exist, proceed to insert
@@ -48,7 +94,7 @@ async function run() {
           console.log(result);
           res
             .status(201)
-            .send({ message: "User inserted", userId: result.insertedId });
+            .send({ token });
         }
       } catch (error) {
         console.error(error);
@@ -93,13 +139,13 @@ async function run() {
       }
     });
 
-    app.patch("/user/:id", async (req, res) => {
+    app.patch("/user/:id", verifyToken, async (req, res) => {
       try {
         const userId = req.params.id;
         const updateData = req.body;
 
         if (!ObjectId.isValid(userId)) {
-          return res.status(400).send({ message: "Invalid book ID format" });
+          return res.status(400).send({ message: "Invalid user ID format" });
         }
 
         const result = await userCollection.updateOne(
@@ -118,7 +164,130 @@ async function run() {
       }
     });
 
-    // app.get("/user/:id", async (req, res) => {
+   
+
+    // books
+    app.post("/books", verifyToken, async (req, res) => {
+      try {
+        const book = req.body;
+        const result = await booksCollection.insertOne(book);
+        res
+          .status(201)
+          .send({ message: "Book inserted", bookId: result.insertedId });
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Error inserting book" });
+      }
+    });
+
+    // GET route to fetch all books
+    app.get("/books", async (req, res) => {
+      try {
+        const books = await booksCollection.find().toArray();
+        res.status(200).send(books);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Error fetching books" });
+      }
+    });
+
+    // update book
+    app.patch("/books/:id", verifyToken, async (req, res) => {
+      try {
+        const bookId = req.params.id;
+        const updateData = req.body;
+
+        if (!ObjectId.isValid(bookId)) {
+          return res.status(400).send({ message: "Invalid book ID format" });
+        }
+
+        const result = await booksCollection.updateOne(
+          { _id: new ObjectId(bookId) },
+          { $set: updateData }
+        );
+
+        if (result.matchedCount === 0) {
+          return res.status(404).send({ message: "Book not found" });
+        }
+
+        res.status(200).send({ message: "Book updated successfully" });
+      } catch (error) {
+        console.error("Error updating book:", error);
+        res.status(500).send({ message: "Error updating book" });
+      }
+    });
+
+    app.get("/books/:id",  async (req, res) => {
+      try {
+        const bookId = req.params.id;
+        const book = await booksCollection.findOne({
+          _id: new ObjectId(bookId),
+        });
+
+        if (!book) {
+          res.status(404).send({ message: "Book not found" });
+        } else {
+          res.status(200).send(book);
+        }
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Error fetching book" });
+      }
+    });
+
+    app.delete("/books/:id",  verifyToken, async (req, res) => {
+      try {
+        const bookId = req.params.id;
+
+        if (!ObjectId.isValid(bookId)) {
+          return res.status(400).send({ message: "Invalid book ID format" });
+        }
+
+        const result = await booksCollection.deleteOne({
+          _id: new ObjectId(bookId),
+        });
+
+        if (result.deletedCount === 0) {
+          return res.status(404).send({ message: "Book not found" });
+        }
+
+        res.status(200).send({ message: "Book deleted successfully" });
+      } catch (error) {
+        console.error("Error deleting book:", error);
+        res.status(500).send({ message: "Error deleting book" });
+      }
+    });
+
+    // categories
+    app.get("/categories", async (req, res) => {
+      try {
+        const categories = await booksCollection.find().toArray();
+        res.status(200).send(categories);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Error fetching categories" });
+      }
+    });
+
+    console.log("You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  } finally {
+    // Uncomment this line if you want to close the connection after starting the server
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
+module.exports = app;
+
+
+ // app.get("/user/:id", async (req, res) => {
     //   try {
     //     // const id = decodeURIComponent(req.params.id);
     //     const id = req.params.id;
@@ -220,122 +389,3 @@ async function run() {
     //     res.status(500).send({ message: "Error inserting user" });
     //   }
     // });
-
-    // books
-    app.post("/books", async (req, res) => {
-      try {
-        const book = req.body;
-        const result = await booksCollection.insertOne(book);
-        res
-          .status(201)
-          .send({ message: "Book inserted", bookId: result.insertedId });
-      } catch (error) {
-        console.error(error);
-        res.status(500).send({ message: "Error inserting book" });
-      }
-    });
-
-    // GET route to fetch all books
-    app.get("/books", async (req, res) => {
-      try {
-        const books = await booksCollection.find().toArray();
-        res.status(200).send(books);
-      } catch (error) {
-        console.error(error);
-        res.status(500).send({ message: "Error fetching books" });
-      }
-    });
-
-    app.patch("/books/:id", async (req, res) => {
-      try {
-        const bookId = req.params.id;
-        const updateData = req.body;
-
-        if (!ObjectId.isValid(bookId)) {
-          return res.status(400).send({ message: "Invalid book ID format" });
-        }
-
-        const result = await booksCollection.updateOne(
-          { _id: new ObjectId(bookId) },
-          { $set: updateData }
-        );
-
-        if (result.matchedCount === 0) {
-          return res.status(404).send({ message: "Book not found" });
-        }
-
-        res.status(200).send({ message: "Book updated successfully" });
-      } catch (error) {
-        console.error("Error updating book:", error);
-        res.status(500).send({ message: "Error updating book" });
-      }
-    });
-
-    app.get("/books/:id", async (req, res) => {
-      try {
-        const bookId = req.params.id;
-        const book = await booksCollection.findOne({
-          _id: new ObjectId(bookId),
-        });
-
-        if (!book) {
-          res.status(404).send({ message: "Book not found" });
-        } else {
-          res.status(200).send(book);
-        }
-      } catch (error) {
-        console.error(error);
-        res.status(500).send({ message: "Error fetching book" });
-      }
-    });
-
-   
-
-    app.delete("/books/:id", async (req, res) => {
-      try {
-        const bookId = req.params.id;
-
-        if (!ObjectId.isValid(bookId)) {
-          return res.status(400).send({ message: "Invalid book ID format" });
-        }
-
-        const result = await booksCollection.deleteOne({
-          _id: new ObjectId(bookId),
-        });
-
-        if (result.deletedCount === 0) {
-          return res.status(404).send({ message: "Book not found" });
-        }
-
-        res.status(200).send({ message: "Book deleted successfully" });
-      } catch (error) {
-        console.error("Error deleting book:", error);
-        res.status(500).send({ message: "Error deleting book" });
-      }
-    });
-
-    // categories
-    app.get("/categories", async (req, res) => {
-      try {
-        const categories = await booksCollection.find().toArray();
-        res.status(200).send(categories);
-      } catch (error) {
-        console.error(error);
-        res.status(500).send({ message: "Error fetching categories" });
-      }
-    });
-
-    console.log("You successfully connected to MongoDB!");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  } finally {
-    // Uncomment this line if you want to close the connection after starting the server
-    // await client.close();
-  }
-}
-
-run().catch(console.dir);
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
